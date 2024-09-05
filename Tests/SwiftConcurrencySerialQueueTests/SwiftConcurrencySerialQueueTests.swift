14880394861abe2ca7969e12afe87611f0823ff9
Doug Russell <doug@getitdownonpaper.com>
2024-09-05 11:16:25 -0400

    Tests for non throwing queue variant. General cleanup.

--- Tests/SwiftConcurrencySerialQueueTests/SwiftConcurrencySerialQueueTests.swift
+++ Tests/SwiftConcurrencySerialQueueTests/SwiftConcurrencySerialQueueTests.swift
@@ -2,30 +2,60 @@ import Testing
 @testable import SwiftConcurrencySerialQueue
 
 @available(macOS 14.0, iOS 17.0, watchOS 10.0, tvOS 17.0, *)
-@Test func countOrderingWithoutQueue() async throws {
+@Test func withoutQueue() async throws {
     // This flakes on run repeatedly
     // Changing Task.yield in TestActor to Task.sleep makes it flake *less*
     let actor = TestActor()
     try await withThrowingTaskGroup(of: Void.self) { group in
         group.addTask(priority: .high) {
-            try await actor.incrementWithSuspension()
+            #expect(await actor.incrementWithSuspension())
         }
         group.addTask(priority: .low) {
-            try await actor.increment()
+            await actor.increment()
         }
         try await group.waitForAll()
     }
 }
 
 @available(macOS 14.0, iOS 17.0, watchOS 10.0, tvOS 17.0, *)
-@Test func countOrderingWithQueue() async throws {
+@Test func withQueue() async throws {
     let actor = TestActor()
     try await withThrowingTaskGroup(of: Void.self) { group in
         group.addTask(priority: .high) {
-            try await actor.incrementWithSuspensionOrdered()
+            #expect(await actor.incrementWithSuspension_Ordered())
         }
         group.addTask(priority: .low) {
-            try await actor.incrementOrdered()
+            await actor.increment_Ordered()
+        }
+        try await group.waitForAll()
+    }
+}
+
+@available(macOS 14.0, iOS 17.0, watchOS 10.0, tvOS 17.0, *)
+@Test func withoutQueue_Throwing() async throws {
+    // This flakes on run repeatedly
+    // Changing Task.yield in TestActor to Task.sleep makes it flake *less*
+    let actor = TestActor()
+    try await withThrowingTaskGroup(of: Void.self) { group in
+        group.addTask(priority: .high) {
+            try await actor.incrementWithSuspension_Throwing()
+        }
+        group.addTask(priority: .low) {
+            try await actor.increment_Throwing()
+        }
+        try await group.waitForAll()
+    }
+}
+
+@available(macOS 14.0, iOS 17.0, watchOS 10.0, tvOS 17.0, *)
+@Test func withQueue_Throwing() async throws {
+    let actor = TestActor()
+    try await withThrowingTaskGroup(of: Void.self) { group in
+        group.addTask(priority: .high) {
+            try await actor.incrementWithSuspension_Ordered_Throwing()
+        }
+        group.addTask(priority: .low) {
+            try await actor.increment_Ordered_Throwing()
         }
         try await group.waitForAll()
     }
--- Tests/SwiftConcurrencySerialQueueTests/TestActor.swift
+++ Tests/SwiftConcurrencySerialQueueTests/TestActor.swift
@@ -4,39 +4,75 @@ import Testing
 @available(macOS 14.0, iOS 17.0, watchOS 10.0, tvOS 17.0, *)
 actor TestActor {
     private var count = 0
-    func incrementWithSuspension() async throws {
-        count += 1
-        let before = count
-        await Task.yield()
-        //try await Task.sleep(for: .milliseconds(1200))
-        #expect(count != before)
-    }
-    func increment() async throws {
-        count += 1
-    }
-    private let queue = SerialQueueThrowing()
-    func incrementWithSuspensionOrdered() async throws {
-        try await self.queue.enqueue {
-            try await self._incrementWithSuspensionOrdered()
-        }
-    }
-    func _incrementWithSuspensionOrdered() async throws {
-        self.count += 1
-        let before = count
-        await Task.yield()
-        //try await Task.sleep(for: .milliseconds(1200))
-        #expect(count == before)
-    }
-    func incrementOrdered() async throws {
-        try await self.queue.enqueue {
-            try await self._incrementOrdered()
-        }
-    }
-    func _incrementOrdered() async throws {
-        count += 1
-    }
+    private let queueThrowing = SerialQueueThrowing()
+    private let queue = SerialQueue()
     private nonisolated let executor: any SerialExecutor
     nonisolated let unownedExecutor: UnownedSerialExecutor
+
+    func incrementWithSuspension() async -> Bool {
+        count += 1
+        let before = count
+        await suspension()
+        return count != before
+    }
+    func increment() async {
+        count += 1
+    }
+
+    func incrementWithSuspension_Throwing() async throws {
+        count += 1
+        let before = count
+        await suspension()
+        #expect(count != before)
+    }
+    func increment_Throwing() async throws {
+        count += 1
+    }
+
+    func incrementWithSuspension_Ordered_Throwing() async throws {
+        try await self.queueThrowing.enqueue {
+            try await self._incrementWithSuspension_Ordered_Throwing()
+        }
+    }
+    private func _incrementWithSuspension_Ordered_Throwing() async throws {
+        self.count += 1
+        let before = count
+        await suspension()
+        #expect(count == before)
+    }
+    func increment_Ordered_Throwing() async throws {
+        try await self.queueThrowing.enqueue {
+            await self._increment_Ordered()
+        }
+    }
+
+    func incrementWithSuspension_Ordered() async -> Bool {
+        await self.queue.enqueue {
+            await self._incrementWithSuspension_Ordered()
+        }
+    }
+    private func _incrementWithSuspension_Ordered() async -> Bool {
+        self.count += 1
+        let before = count
+        await suspension()
+        return count == before
+    }
+    func increment_Ordered() async {
+        await self.queue.enqueue {
+            await self._increment_Ordered()
+        }
+    }
+    private func _increment_Ordered() async {
+        count += 1
+    }
+    
+    private func suspension() async {
+        await Task.yield()
+        //do {
+        //    try await Task.sleep(for: .milliseconds(1200))
+        //} catch {}
+    }
+
     init() {
         let executor = TestExecutor()
         self.executor = executor
